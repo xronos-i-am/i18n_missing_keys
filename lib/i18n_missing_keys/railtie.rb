90e640c74da60c3e7d1e23cde890595896c34b37
ideadapt <kunz@ideadapt.net>
2013-02-25 19:05:30 +0100

    setup railties in progress

--- lib/i18n_missing_keys/railtie.rb
+++ lib/i18n_missing_keys/railtie.rb
@@ -3,7 +3,11 @@ require 'rails'
 module I18nMissingKeys
   class Railtie < Rails::Railtie
     rake_tasks do
-      import 'lib/tasks/i18n_missing_keys'
+      #TODO require fails
+      # http://blog.nathanhumbert.com/2010/02/rails-3-loading-rake-tasks-from-gem.html
+      # https://blog.engineyard.com/2010/extending-rails-3-with-railties
+      # http://stackoverflow.com/questions/742633/make-rake-task-from-gem-available-everywhere
+      require 'lib/tasks/i18n_missing_keys.rake'
     end
   end
 end
