8f75e6252f7fbcdf1cb96709eb29ec58a12597f6
ideadapt <kunz@ideadapt.net>
2013-02-25 18:52:51 +0100

    setup railties

--- lib/i18n_missing_keys/railtie.rb
+++ lib/i18n_missing_keys/railtie.rb
@@ -3,7 +3,7 @@ require 'rails'
 module I18nMissingKeys
   class Railtie < Rails::Railtie
     rake_tasks do
-      require 'tasks/i18n_missing_keys.rake'
+      require '../tasks/i18n_missing_keys.rake'
     end
   end
 end
