007f2cec9b3d4bdc29555a19163703e590fce8fa
ideadapt <kunz@ideadapt.net>
2013-02-25 21:20:03 +0100

    load

--- lib/i18n_missing_keys/railtie.rb
+++ lib/i18n_missing_keys/railtie.rb
@@ -7,7 +7,8 @@ module I18nMissingKeys
       # http://blog.nathanhumbert.com/2010/02/rails-3-loading-rake-tasks-from-gem.html
       # https://blog.engineyard.com/2010/extending-rails-3-with-railties
       # http://stackoverflow.com/questions/742633/make-rake-task-from-gem-available-everywhere
-      require 'lib/tasks/i18n_missing_keys.rake'
+      #require 'lib/tasks/i18n_missing_keys.rake'
+      load File.expand_path('../tasks/i18n_missing_keys.rake', __FILE__)
     end
   end
 end
